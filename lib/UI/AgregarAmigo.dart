33ef97b4f1a530a3968950040437c4697ff61545
Jean Palacio <jeanpalacio@uninorte.edu.co>
2020-06-09 06:22:14 -0500

    Buscar&Agregar amigos+guardar en Firestore

--- lib/UI/AgregarAmigo.dart
+++ lib/UI/AgregarAmigo.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:proyecto_final/Models/Persona.dart';
+import 'package:proyecto_final/Services/FireDatabase.dart';
+import 'package:proyecto_final/UI/AgregarAmigoCard.dart';
 
 class AgregarAmigo extends StatefulWidget {
   @override
@@ -6,33 +10,72 @@ class AgregarAmigo extends StatefulWidget {
 }
 
 class _AgregarAmigoState extends State<AgregarAmigo> {
+  List<Persona> resbusqueda = new List<Persona>();
+  QuerySnapshot snapbusq;
+  bool encontrado = false;
   TextEditingController busqueda = new TextEditingController();
   @override
+  DatabaseThings fireDB = new DatabaseThings();
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
-          AppBar(title: Text("School Ready!"), backgroundColor: Colors.black),
+          AppBar(title: Text("Traeme algo!"), backgroundColor: Colors.red[400]),
       body: Column(
         children: [
           Container(
               color: Colors.black26,
               padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-              child: Row(
+              child: Column(
                 children: <Widget>[
-                  Expanded(
-                      child: TextField(
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: TextField(
                         controller: busqueda,
-                        
-                    decoration: InputDecoration(
-                        hintText: "Buscar usuarios",
-                        hintStyle: TextStyle(color: Colors.white),
-                        border: InputBorder.none),
-                  )),
-                  FlatButton(onPressed: null, child: Row(children: <Widget>[Icon(Icons.search),Text("Buscar")],))
+                        decoration: InputDecoration(
+                            hintText: "Buscar usuarios",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: InputBorder.none),
+                      )),
+                      FlatButton(
+                          color: Colors.grey[200],
+                          shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                          onPressed: () { buscar();},
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.search),
+                              Text("Buscar")
+                            ],
+                          ))
+                    ],
+                  ),
+                 
                 ],
               ))
-        ],
-      ),
+        ,_listUsuarios()],
+       ),
     );
+  }
+
+  void buscar() {
+    fireDB.buscarporNombre(busqueda.text).then((resbu) {
+      setState(() {
+        snapbusq = resbu;  
+      });
+      
+    });
+  }
+
+  Widget _listUsuarios() {
+    return snapbusq != null? 
+        ListView.builder(
+        shrinkWrap: true,
+        itemCount: snapbusq.documents.length,
+        itemBuilder: (context, posicion) {
+          return AgregarAmigoCard(snapbusq.documents[posicion].data["nombre"],
+              snapbusq.documents[posicion].data["Nombre de usuario"],
+              snapbusq.documents[posicion].documentID,snapbusq.documents[posicion].data["email"]);
+        }): Container();
   }
 }
--- lib/UI/Registro.dart
+++ lib/UI/Registro.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:proyecto_final/Models/MultipleKeys.dart';
+import 'package:proyecto_final/Services/FireAuth.dart';
+import 'package:proyecto_final/Services/FireDatabase.dart';
+import 'package:proyecto_final/Services/RequestStructure.dart';
 import 'package:string_validator/string_validator.dart';
 
 var globalContext;
@@ -31,13 +34,16 @@ class Registrarform extends StatefulWidget {
   }
 }
 
-
 class RegistrarformState extends State {
   //final GlobalKey<FormState> _signUpfkey = GlobalKey<FormState>();
+  FireAuth fireReg = new FireAuth();
+  DatabaseThings fireDB = new DatabaseThings();
   final _email = new TextEditingController();
   final _password = new TextEditingController();
   final _name = new TextEditingController();
   final _username = new TextEditingController();
+  final _city = new TextEditingController();
+  final _age = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -54,6 +60,7 @@ class RegistrarformState extends State {
               TextFormField(
                 autofocus: true,
                 controller: _email,
+                keyboardType: TextInputType.emailAddress,
                 decoration: new InputDecoration(
                   labelText: "Email",
                   labelStyle: TextStyle(color: Colors.white),
@@ -61,7 +68,56 @@ class RegistrarformState extends State {
                   hintStyle: TextStyle(color: Colors.white),
                 ),
               ),
-            ),            
+            ),
+            containerText(
+              TextFormField(
+                autofocus: true,
+                controller: _name,
+                decoration: new InputDecoration(
+                  labelText: "Nombre",
+                  labelStyle: TextStyle(color: Colors.white),
+                  hintText: "nombre",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            containerText(
+              TextFormField(
+                autofocus: true,
+                controller: _username,
+                decoration: new InputDecoration(
+                  labelText: "Nombre de usuario",
+                  labelStyle: TextStyle(color: Colors.white),
+                  hintText: "nomobre112",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            containerText(
+              TextFormField(
+                autofocus: true,
+                controller: _age,
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(
+                  labelText: "Edad",
+                  labelStyle: TextStyle(color: Colors.white),
+                  hintText: "56",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            containerText(
+              TextFormField(
+                autofocus: true,
+                controller: _city,
+                decoration: new InputDecoration(
+                  labelText: "Ciudad",
+                  labelStyle: TextStyle(color: Colors.white),
+                  hintText: "Barranquilla",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
             containerText(TextFormField(
               key: MultipleKeys.signUpFormKey,
               autofocus: true,
@@ -86,7 +142,7 @@ class RegistrarformState extends State {
                   onpressedregistar();
                 } else {
                   Scaffold.of(globalContext)
-                      .showSnackBar(SnackBar(content: Text('Invalid Email')));
+                      .showSnackBar(SnackBar(content: Text('Email invalido')));
                 }
                 Navigator.pop(globalContext);
               },
@@ -96,14 +152,29 @@ class RegistrarformState extends State {
   }
 
   void onpressedregistar() async {
-    final AuthResult user = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(
-            email: _email.text, password: _password.text);
-            if(user!=null){
-              Navigator.pop(context);
-              Scaffold.of(context)
-          .showSnackBar(SnackBar(content: Text('Registered')));
-            }
+    bool resp = false;
+    if (_password.text.length >= 6) {
+      Map<String, String> mapadeluser = {
+        "edad": _age.text,
+        "Nombre de usuario": _username.text,
+        "nombre": _name.text,
+        "email": _email.text,
+        "ciudad": _city.text
+      };
+      fireReg
+          .registrarNuevoUsuario(_email.text, _password.text, context)
+          .then((res) {
+        if (res != null) {
+          fireDB.subirinfoUsuario(mapadeluser);
+          Navigator.pop(context);
+          print("registro exitoso");
+        } else {
+          print("error en el registro");
+        }
+      });
+    } else {
+      print("contraseña debe ser de minimo 6 caracteres");
+    }
   }
 
   Widget containerText(Widget widg) {
